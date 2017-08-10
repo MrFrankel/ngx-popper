e91f3d5ff26c73241f0547ada3ef2891adf4648e
MrFrankel <maorfrank@gmail.com>
2017-08-10 11:43:00 +0300

    fix runtime error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {NgxPopperModule} from './popper.module';
-
 import 'popper.js';
 export * from './popper';
 export * from './popper.model';
 export * from './popper-content';
 export * from './popper.module';
-platformBrowserDynamic().bootstrapModule(NgxPopperModule);
